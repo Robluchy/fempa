40ef634a78f809c2c59b1873b9c51cd8fe98bab1
Fer457 <fernandonevothernandez@gmail.com>
2022-12-19 22:44:01 +0100

    initial commit

---
+++ src/main/java/proyecto/fempa/FempaApplication.java
@@ -0,0 +1,13 @@
+package proyecto.fempa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FempaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FempaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/fempa/FempaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.fempa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FempaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
